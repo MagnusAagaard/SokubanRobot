b0098e61bec300a44abe8fdf71b2f2683dc04c58
Magnus Aagaard <jaaag17@student.sdu.dk>
2020-10-22 16:14:36 +0200

    Turn 180 and go backwards

--- Robot_behaviour/robot.py
+++ Robot_behaviour/robot.py
@@ -7,12 +7,15 @@ from ev3dev2.wheel import EV3Tire
 import signal
 
 class LegoRobot:
-	def __init__(self):
+	def __init__(self, solution):
 		self._setup_sensors()
 		self._setup_motors()
 		self._setup_shutdowns()
 		self.white = 76
 		self.black = 5
+		self.solution = solution
+		#Orientations: 1 = up, 2 = right, 3 = down, 4 = left
+		self.robot_orientation = 1
 
 	def _setup_sensors(self):
 		self.mA = LargeMotor('outA')
@@ -72,22 +75,34 @@ class LegoRobot:
 		self._setup_motors()
 
 	def turn_right(self):
+		self.go_straight(29)
 		self.mdiff.turn_right(self.TURN_SPEED, 90)
 		self._setup_motors()
 
 	def turn_left(self):
+		self.go_straight(35)
 		self.mdiff.turn_left(self.TURN_SPEED, 90)
 		self._setup_motors()
 
+	def turn_around(self):
+		self.mdiff.turn_left(self.TURN_SPEED, 180)
+		self._setup_motors()
+
 if __name__ == "__main__":
-	lr = LegoRobot()
+	lr = LegoRobot("Sol")
 	cross_detected = False
+	cross_detected2 = False
 	while True:
 		lr.check_touch()
 		lr.follow_line()
-		print("Following line!")
 		cross_detected = lr.check_cross()
 		if cross_detected:
-			lr.go_straight(29)
-			lr.turn_right()
-			print("stuck")
+			lr.go_straight(-280)
+			#lr.turn_around()
+			while True:
+				lr.check_touch()
+				lr.follow_line()
+				cross_detected2 = lr.check_cross()
+				if cross_detected2:
+					lr.turn_left()
+
