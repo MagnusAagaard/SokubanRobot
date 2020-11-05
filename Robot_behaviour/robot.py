b0617d99ac9989c6496449cdc647aec83f4224ee
Soeren Madsen <soren.madsen97@gmail.com>
2020-11-05 16:30:40 +0100

    Update robot.py
    With two light sensors next to each other

--- Robot_behaviour/robot.py
+++ Robot_behaviour/robot.py
@@ -5,6 +5,7 @@ from ev3dev2.motor import OUTPUT_A, OUTPUT_D, MoveDifferential, SpeedRPM
 from ev3dev2.wheel import EV3Tire
 
 import signal
+from time import sleep
 
 class LegoRobot:
 	def __init__(self, solution):
@@ -17,8 +18,9 @@ class LegoRobot:
 		self.simplify()
 		#Orientations: 1 = up, 2 = right, 3 = down, 4 = left
 		self.robot_orientation = 1
-		self.straight_left = 26
-		self.straight_right = 36
+		self.straight_left = 20 #26
+		self.straight_right = 30 #36
+		self.threshold = (self.white-self.black)/2-5
 
 	def _setup_sensors(self):
 		self.mA = LargeMotor('outA')
@@ -57,13 +59,12 @@ class LegoRobot:
 			#self.check_touch()
 			self.sensorLeft = self.lightSensorLeft.value()
 			self.sensorRight = self.lightSensorRight.value()
-			threshold = (self.white-self.black)/2-5
 			if not self.check_cross():
 				cross_left=True
 			if cross_left:
 				cross_detected = self.check_cross()
-			self.mD.duty_cycle_sp = self.BASE_SPEED - 0.20*(self.sensorLeft-threshold)
-			self.mA.duty_cycle_sp = self.BASE_SPEED + 0.20*(self.sensorLeft-threshold)
+			self.mD.duty_cycle_sp = self.BASE_SPEED - 0.20*(self.sensorLeft-self.threshold)
+			self.mA.duty_cycle_sp = self.BASE_SPEED + 0.20*(self.sensorLeft-self.threshold)
 
 
 	def check_touch(self):
@@ -75,7 +76,7 @@ class LegoRobot:
 			exit(0)
 
 	def check_cross(self):
-		if self.sensorRight < self.black + 37:
+		if self.sensorRight < self.black + 25 and self.sensorLeft < self.black + 25:
 			return True
 		else:
 			return False
@@ -90,6 +91,27 @@ class LegoRobot:
 		self.mdiff.turn_right(self.TURN_SPEED, 90)
 		self._setup_motors()
 
+	def turn_right_new(self):
+		dist=self.straight_right
+		self.go_straight(dist)
+		self.mD.duty_cycle_sp = -40
+		self.mA.duty_cycle_sp = 40
+		sleep(0.4)
+		while self.lightSensorRight.value()>self.threshold:
+			sleep(0.01)
+		sleep(0.03)
+
+	def turn_left_new(self):
+		dist=self.straight_right
+		self.go_straight(dist)
+		self.mD.duty_cycle_sp = 40
+		self.mA.duty_cycle_sp = -40
+		sleep(0.4)
+		while self.lightSensorLeft.value()>self.threshold+10:
+			sleep(0.01)
+		sleep(0.03)
+
+
 	def turn_left(self):
 		dist=self.straight_left
 		self.go_straight(dist)
@@ -97,8 +119,16 @@ class LegoRobot:
 		self._setup_motors()
 
 	def turn_around(self):
-		self.mdiff.turn_left(self.TURN_SPEED, 180)
-		self._setup_motors()
+		self.mD.duty_cycle_sp = 40
+		self.mA.duty_cycle_sp = -40
+		sleep(0.4)
+		while self.lightSensorLeft.value()>self.threshold:
+			sleep(0.01)
+		sleep(0.03)
+		sleep(0.4)
+		while self.lightSensorLeft.value()>self.threshold:
+			sleep(0.01)
+		sleep(0.03)
 	
 	def move_can(self):
 		self.go_straight(195)
@@ -113,7 +143,7 @@ class LegoRobot:
 			ori_change = desired_ori - self.robot_orientation
 			self.robot_orientation = desired_ori
 			if (ori_change == 1 or ori_change == -3):
-				self.turn_right()
+				self.turn_right_new()
 				self.follow_line()
 				print ("turning right")
 			elif (ori_change == 2 or ori_change == -2):
@@ -121,7 +151,7 @@ class LegoRobot:
 				self.follow_line()
 				print("180 no scope")
 			elif (ori_change == -1 or ori_change == 3):
-				self.turn_left()
+				self.turn_left_new()
 				self.follow_line()
 				print("turning left")
 			else:
@@ -149,6 +179,8 @@ class LegoRobot:
 
 if __name__ == "__main__":
 	#Hvis problemer, rens hjul og lad batteriet fuldt op
+
+	#Næste gang, lav vores straight om til at bruge noget andet til at køre frem. Enten sæt lys sensor ved aksen, eller lav med tid.
 	lr = LegoRobot("llllUddlluRRRRRdrUUruulldRRlddlluLuulldRurDDullDRdRRRdrUUruurrdLulDulldRddlllldlluRRRRRdrUUdlllluurDldRRRdrU")
 #	lr = LegoRobot("uldruldruldruldruldruldruldruldruldruldruldruldruldruldr") #left test
 #	lr = LegoRobot("rdlurdlurdlurdlurdlurdlurdlurdlurdlurdlurdlurdlurdlurdlu") #right test
@@ -157,6 +189,7 @@ if __name__ == "__main__":
 	lr.mA.duty_cycle_sp = 0
 	lr.mD.duty_cycle_sp = 0
 	exit(0)
+#	lr.turn_right_new()
 #	while True:
 #		lr.check_touch()
 #		lr.follow_line()
